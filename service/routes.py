ba9a52c4c918a00254129ff6962dac179b76f4c7
Tom Z <78955612+tzim999@users.noreply.github.com>
2024-09-15 16:46:38 -0700

    Update routes.py
    format fixes

--- service/routes.py
+++ service/routes.py
@@ -89,7 +89,8 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    return account.serialize(), status.HTTP_200_OK      
+    return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -106,25 +107,10 @@ def update_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-@app.route("/accounts/<int:account_id>", methods=["PUT"])
-def update_accounts(account_id):
-    """
-    Update an Account
-    This endpoint will update an Account based on the posted data
-    """
-    app.logger.info("Request to update an Account with id: %s", account_id)
-
-    account = Account.find(account_id)
-    if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
-    account.deserialize(request.get_json())
-    account.update()
-
     return account.serialize(), status.HTTP_200_OK
 
 
-  ######################################################################
+######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
@@ -134,10 +120,10 @@ def delete_accounts(account_id):
     This endpoint will delete an Account based on the account_id that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     if account:
         account.delete()
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
